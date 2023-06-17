d8534abf20a7428e270656c22d10f2980eee07e1
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-06-17 18:01:06 +0530

    Configure next auth

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,6 +1,6 @@
 // Inspired by react-hot-toast library
 import * as React from "react"
-import type { ToastActionElement, ToastProps } from "@/components/UI/toast"
+import type { ToastActionElement, ToastProps } from "@/components/UI/Toast"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
---
+++ src/libs/auth.ts
@@ -0,0 +1,59 @@
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import { nanoid } from "nanoid";
+import db from "./db";
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(db),
+    session: {
+        strategy: "jwt",
+    },
+    pages: {
+        signIn: "/siginin"
+    },
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_SECRET_PASSWORD!
+        })
+    ],
+    callbacks: {
+        async session({ token, session }) {
+            if (token) {
+                session.user.id = token.id;
+                session.user.name = token.name;
+                session.user.email = token.email;
+                session.user.image = token.picture;
+                session.user.username = token.username;
+            }
+            return session;
+        },
+        async jwt({ token, user }) {
+            const dbUser = await db.user.findFirst({
+                where: {
+                    email: token.email
+                }
+            })
+            if (!dbUser) {
+                token.id = user.id;
+                return token;
+            }
+            if (!dbUser.username) {
+                db.user.update({
+                    where: { id: dbUser.id },
+                    data: { username: nanoid(10) }
+                })
+            }
+
+            return {
+                id: dbUser.id,
+                name: dbUser.name,
+                email: dbUser.email,
+                picture: dbUser.image,
+                username: dbUser.username,
+            }
+        },
+        redirect: () => "/"
+    }
+};
