d77348737d9dd5246ce930180ea871145326d05a
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-07-09 01:03:37 +0530

    feat: create sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,34 @@
+import { type MetadataRoute } from "next"
+
+import siteConfig from "@/config"
+import database from "@/lib/database"
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+
+    const postsData = await database.post.findMany();
+    const forumData = await database.forum.findMany();
+
+    const posts = postsData.map((post) => ({
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        url: `${siteConfig.url}/d/${forumData.find(forum => forum.id === post.forumId)!.name}/${post.id}`,
+        lastModified: post.updatedAt.toISOString()
+    }))
+
+    const forums = forumData.map(forum => ({
+        url: `${siteConfig.url}/d/${forum.name}`,
+        lastModified: forum.updatedAt.toISOString()
+    }))
+
+    const routes = [
+        "",
+        "/home",
+        "/leaderboard",
+    ].map((route) => ({
+        url: `${siteConfig.url}${route}`,
+        lastModified: new Date().toISOString(),
+    }))
+
+    console.log([...routes, ...posts, ...forums]);
+
+    return [...routes, ...posts, ...forums]
+}
