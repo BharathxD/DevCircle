d27426a23e3d4ec1e28a51f1cd2204a32ea38b62
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-07-15 00:33:02 +0530

    fix: add twitter og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [siteConfig.url],
+    images: [`${siteConfig.url}/og.png`],
     creator: "@Bharath_uwu",
   },
   icons: {
