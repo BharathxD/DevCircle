3e440c2a2b87690fc2b9a0898ffe6274e25d4e41
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-07-15 16:22:00 +0530

    chore: refactor post cache deletion logic

--- src/helpers/deletePostCache.ts
+++ src/helpers/deletePostCache.ts
@@ -1,6 +1,11 @@
 import redis from "@/lib/redis";
 
-const deletePostCache = async (postId: string) =>
+/**
+ * Deletes the cache for a specific post.
+ * @param postId - ID of the post to delete the cache for.
+ */
+const deletePostCache = async (postId: string) => {
   await redis.del(`post:${postId}`);
+};
 
 export default deletePostCache;
