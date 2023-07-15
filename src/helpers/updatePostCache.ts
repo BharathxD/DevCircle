94e3758ff9ef4cfd488f6694aa8c04edec787834
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-07-15 16:21:02 +0530

    chore: refactor post cache update logic

--- src/helpers/updatePostCache.ts
+++ src/helpers/updatePostCache.ts
@@ -3,16 +3,19 @@ import type { Post, Tag, User, Vote } from "@prisma/client";
 import type { CachedPost } from "@/types/redis";
 import redis from "@/lib/redis";
 
-type updatePostCacheParams = Post & {
+/**
+ * Updates the cached post information.
+ * @param post - Post data to update the cache.
+ */
+async function updatePostCache(post: Post & {
   votes: Vote[];
   author: User;
   tags: Tag[];
-};
-
-async function updatePostCache(post: updatePostCacheParams) {
+}) {
   const existingCachedPayload: CachedPost | null = await redis.hgetall(
     `post:${post.id}`
   );
+
   const updatedCachedPayload: CachedPost = {
     id: post.id,
     title: post.title,
@@ -24,6 +27,7 @@ async function updatePostCache(post: updatePostCacheParams) {
     currentVote: existingCachedPayload?.currentVote || null,
     createdAt: post.createdAt,
   };
+
   await redis.hmset(`post:${post.id}`, updatedCachedPayload);
 }
 
