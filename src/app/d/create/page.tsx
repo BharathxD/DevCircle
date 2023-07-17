19e6aef72fcd93ac6b7bdf13ce856941deeb6e46
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-07-17 12:59:05 +0530

    fix: bg gradient in forum creation page

--- src/app/d/create/page.tsx
+++ src/app/d/create/page.tsx
@@ -77,8 +77,8 @@ const CreatePostPage: React.FC = () => {
     input.length >= 3 && description.length >= 10 && !/\s/.test(input);
 
   return (
-    <div className="mt-4 md:mt-0 md:flex md:h-[70vh] md:w-full md:items-center md:justify-center">
-      <div className="flex h-full w-full items-center md:container md:mx-auto md:max-w-3xl">
+    <div className="mt-4 h-[91vh] md:mt-0 md:flex md:w-full md:items-center md:justify-center">
+      <div className="flex h-full w-full items-start md:container md:mx-auto md:max-w-3xl md:items-center">
         <section className="w-full rounded-lg border-2 border-zinc-800 bg-zinc-50 dark:bg-zinc-900">
           <header className="flex items-center justify-between border-b-2 border-b-zinc-800 p-5 ">
             <h1 className="text-2xl font-bold">Create a Community</h1>
