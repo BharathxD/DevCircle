74dd608a54f81a2605f7bba2afdd11b1da94131e
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-11-21 22:17:16 +0530

    chore: misc changes

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,12 +2,13 @@ import { redirect } from "next/navigation";
 import { getUserWithSocialLinks } from "@/actions/getCurrentUser";
 
 import ProfileForm from "@/components/forms/profile-form";
+import { Fragment } from "react";
 
 const SettingsProfilePage = async () => {
   const user = await getUserWithSocialLinks();
   if (!user) redirect("/settings/appearance");
   return (
-    <div>
+    <Fragment>
       <div className="px-4 py-2">
         <h3 className="text-xl font-bold">Profile</h3>
         <p className="text-sm text-muted-foreground">
@@ -23,7 +24,7 @@ const SettingsProfilePage = async () => {
           facebook: user.socialMedia?.facebook,
         }}
       />
-    </div>
+    </Fragment>
   );
 };
 
