725eee0a26aa2fd0b813f23a85e1a5f259a1989a
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2024-02-14 00:46:58 +0530

    fix: lag while switching modes

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import type { FC, ReactNode } from "react";
+import type { FC } from "react";
 import { ThemeProvider as TP } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
 
-interface ThemeProviderProps {
-  children: ReactNode;
-}
-
-const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => (
-  <TP attribute="class">{children}</TP>
+const ThemeProvider: FC<React.PropsWithChildren & ThemeProviderProps> = ({
+  children,
+  ...rest
+}) => (
+  <TP attribute="class" disableTransitionOnChange {...rest}>
+    {children}
+  </TP>
 );
 
 export default ThemeProvider;
